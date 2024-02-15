fe0ea683b84e80aecc4a628bbe4430602c5a6092
venkatbhattiprolu <vbhat847@gmail.com>
2024-02-14 23:11:33 -0800

    third-stage

---
+++ terraform-s3-bucket/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state-formydevops-project"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
